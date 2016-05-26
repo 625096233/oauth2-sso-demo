d9923de5f8361ce1a859a6e86df5347fee5a35d0
bilak <bilaak@gmail.com>
2016-05-26 10:20:46 +0200

    - moved configuration classes to configuration package

--- authorization/src/main/java/com/github/bilak/oauth2poc/uaa/configuration/AuthenticationConfiguration.java
+++ authorization/src/main/java/com/github/bilak/oauth2poc/uaa/configuration/AuthenticationConfiguration.java
@@ -1,4 +1,4 @@
-package com.github.bilak.oauth2poc.uaa;
+package com.github.bilak.oauth2poc.uaa.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
--- authorization/src/main/java/com/github/bilak/oauth2poc/uaa/configuration/OAuth2AuthorizationConfig.java
+++ authorization/src/main/java/com/github/bilak/oauth2poc/uaa/configuration/OAuth2AuthorizationConfig.java
@@ -1,4 +1,4 @@
-package com.github.bilak.oauth2poc.uaa;
+package com.github.bilak.oauth2poc.uaa.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
