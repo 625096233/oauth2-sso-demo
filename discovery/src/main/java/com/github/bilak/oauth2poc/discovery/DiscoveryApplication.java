9bb8b501ae63746b5417d906c19525c226b7a1d5
bilak <bilaak@gmail.com>
2016-05-26 10:28:31 +0200

    - added discovery service

---
+++ discovery/src/main/java/com/github/bilak/oauth2poc/discovery/DiscoveryApplication.java
@@ -0,0 +1,17 @@
+package com.github.bilak.oauth2poc.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by lvasek on 26/05/16.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+}
